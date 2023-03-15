584f1ab279218a557a4120ec67df8cc5b3382621
Akhmed <a-kadymov@yandex.ru>
2023-03-15 20:38:36 +0400

    desktop nav menu expanding

--- src/components/nav-menu/nav-menu.js
+++ src/components/nav-menu/nav-menu.js
@@ -12,30 +12,66 @@ export default class NavMenu extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
+            menuExpanded: false
         }
     }
 
+    expandMenu = () => {
+        this.setState({ menuExpanded: true})
+    }
+
+    wrapMenu = () => {
+        this.setState({ menuExpanded: false})
+    }
+
     render() {
+
+        console.log(this.state.onMouseOver)
 
         const ScreenSizeClassPostfix = this.props.isMobile ? "mobile" : (this.props.isDesktop ? "desktop" : "tablet")
         const ScreenOrientationClassPostfix = this.props.isPortrait ? "portrait" : "landscape"
 
         return(
-            <div className={`nav-menu ${ ScreenSizeClassPostfix} ${ ScreenOrientationClassPostfix }`}>
+            <div className={`nav-menu ${ ScreenSizeClassPostfix} ${ ScreenOrientationClassPostfix }`} onMouseOver={this.expandMenu} onMouseLeave={this.wrapMenu}>
                 <div className={`menu-item-container ${ ScreenSizeClassPostfix} ${ ScreenOrientationClassPostfix }`}>
-                    <FeedbackRoundedIcon fontSize="large"/>
+                    <div className="menu-item-icon-container"><FeedbackRoundedIcon fontSize="large"/></div>
+                    {this.state.menuExpanded ? 
+                        <div className="menu-item-title-container"><p className="menu-item-title">FEEDBACK</p></div>   
+                    :
+                        ''
+                    }
                 </div>
                 <div className={`menu-item-container ${ ScreenSizeClassPostfix} ${ ScreenOrientationClassPostfix }`}>
-                    <EmojiEventsRoundedIcon fontSize="large"/>
+                    <div className="menu-item-icon-container"><EmojiEventsRoundedIcon fontSize="large"/></div>
+                    {this.state.menuExpanded ? 
+                        <div className="menu-item-title-container"><p className="menu-item-title">LEADERBOARD</p></div>   
+                    :
+                        ''
+                    }
                 </div>
                 <div className={`menu-item-container ${ ScreenSizeClassPostfix} ${ ScreenOrientationClassPostfix }`}>
-                    <HomeRoundedIcon fontSize="large"/>
+                    <div className="menu-item-icon-container"><HomeRoundedIcon fontSize="large"/></div>
+                    {this.state.menuExpanded ? 
+                        <div className="menu-item-title-container"><p className="menu-item-title">LOBBY</p></div>   
+                    :
+                        ''
+                    }
                 </div>
                 <div className={`menu-item-container ${ ScreenSizeClassPostfix} ${ ScreenOrientationClassPostfix }`}>
-                    <InfoRoundedIcon fontSize="large"/>
+                    <div className="menu-item-icon-container"><InfoRoundedIcon fontSize="large"/></div>
+                    {this.state.menuExpanded ? 
+                        <div className="menu-item-title-container"><p className="menu-item-title">ABOUT</p></div>   
+                    :
+                        ''
+                    }
                 </div>
                 <div className={`menu-item-container ${ ScreenSizeClassPostfix} ${ ScreenOrientationClassPostfix }`}>
-                    <LogoutRoundedIcon fontSize="large"/>
+                    <div className="menu-item-icon-container"><LogoutRoundedIcon fontSize="large"/></div>
+                    {this.state.menuExpanded ? 
+                        <div className="menu-item-title-container"><p className="menu-item-title">SIGN OUT</p></div>   
+                    :
+                        ''
+                    }
                 </div>
             </div>
         )
